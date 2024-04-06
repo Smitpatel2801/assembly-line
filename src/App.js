79ef1b42e5de0959533245f8d1d9d4e566abc4f4
Fwaad Ahmad <68646332+fwaadahmad1@users.noreply.github.com>
2024-04-06 12:53:21 -0400

    backend integrated

--- src/App.js
+++ src/App.js
@@ -1,78 +1,85 @@
-import { Fragment, useState } from "react";
+import {Fragment, useCallback, useEffect, useState} from "react";
 import "./App.css";
 
-import {
-  Card,
-  CardContent,
-  CardContentInfo,
-  CardContentTitle,
-  CardHeaderRed,
-  CardHeading,
-  CardContentValueRed,
-  CardContentValueGreen,
-  CardHeaderGreen,
-  CardContentNormal,
-} from "./App.styles";
-import { calculateRemainingTime } from "./utils";
+import {Card, CardContent, CardContentInfo, CardContentTitle, CardHeaderRed, CardHeading,} from "./App.styles";
+import {calculateRemainingTime} from "./utils";
+
 
 const App = () => {
-  const [assemblyLinesState, setAssemblyLineState] = useState([
-    {
-      name: "A1",
-      status: "PENDING",
-      currentCandy: null,
-      expiryTimeStamp: null,
-    },
-    {
-      name: "A2",
-      status: "PENDING",
-      currentCandy: null,
-      expiryTimeStamp: null,
-    },
-    {
-      name: "A3",
-      status: "PENDING",
-      currentCandy: null,
-      expiryTimeStamp: null,
-    },
-  ]);
 
-  const beautifyTime = (expiryTimeStamp) => {
-    const { hours, minutes, timeDifference } =
-      calculateRemainingTime(expiryTimeStamp);
+    const [assemblyLinesState, setAssemblyLineState] = useState([]);
 
-    if (timeDifference < 0) return "0m";
+    const fetchData = useCallback(async () => {
+        try {
+            const response = await fetch('http://localhost:8000/assembly_line');
+            if (!response.ok) {
+                console.log('Network response was not ok');
+            }
+            const json = await response.json();
+            // console.log(json)
+            const obj = json.map(item => {
+                return {
+                    "name": item?.name,
+                    status: item?.occupied ? "In-Process" : "Open",
+                    currentCandy: item?.candy,
+                    expiryTimeStamp: item?.ending_timestamp,
+                }
+            })
+            setAssemblyLineState(obj)
+            // setAssemblyLineState(obj.map(o => {
+            //     return {
+            //         name: o.name,
+            //         status: o.occupied ? "In-Process" : "Open",
+            //         currentCandy: o.candy,
+            //         expiryTimeStamp: o.ending_timestamp,
+            //     }
+            // }))
+        } catch (error) {
+            console.log(error);
+        }
+    }, [])
 
-    return `${hours}h ${minutes}m `;
-  };
+    useEffect(() => {
+        setInterval(() => {
+            fetchData()
+        }, 1000)
+    }, [])
 
-  return (
-    <Fragment>
-      <Socket />
-      {assemblyLinesState.map((assLine, i) => (
-        <Card key={i} style={{ marginTop: "10px" }}>
-          <CardHeaderRed>
-            <CardHeading>Assembly Line: {assLine.name}</CardHeading>
-          </CardHeaderRed>
-          <CardContent>
-            <CardContentInfo>
-              <CardContentTitle>Status: {assLine.status}</CardContentTitle>
-            </CardContentInfo>
-            <CardContentInfo>
-              <CardContentTitle>
-                Current Candy: {assLine.currentCandy}
-              </CardContentTitle>
-            </CardContentInfo>
-            <CardContentInfo>
-              <CardContentTitle>
-                Time Left: {beautifyTime(assLine.expiryTimeStamp)}
-              </CardContentTitle>
-            </CardContentInfo>
-          </CardContent>
-        </Card>
-      ))}
-    </Fragment>
-  );
+    const beautifyTime = (expiryTimeStamp) => {
+        const {hours, minutes, timeDifference} =
+            calculateRemainingTime(expiryTimeStamp);
+
+        if (timeDifference < 0) return "0m";
+
+        return `${hours}h ${minutes}m `;
+    };
+
+    return (
+        <Fragment>
+            {assemblyLinesState.map((assLine, i) => (
+                <Card key={i} style={{marginTop: "10px"}}>
+                    <CardHeaderRed>
+                        <CardHeading>Assembly Line: {assLine.name}</CardHeading>
+                    </CardHeaderRed>
+                    <CardContent>
+                        <CardContentInfo>
+                            <CardContentTitle>Status: {assLine.status}</CardContentTitle>
+                        </CardContentInfo>
+                        <CardContentInfo>
+                            <CardContentTitle>
+                                Current Candy: {assLine.currentCandy}
+                            </CardContentTitle>
+                        </CardContentInfo>
+                        <CardContentInfo>
+                            <CardContentTitle>
+                                Time Left: {beautifyTime(assLine.expiryTimeStamp)}
+                            </CardContentTitle>
+                        </CardContentInfo>
+                    </CardContent>
+                </Card>
+            ))}
+        </Fragment>
+    );
 };
 
 export default App;
